d7532199b14ae552b6e43831f6687c3b20304c93
Michael M.M <mmontano@alumnes.aulabalear.org>
2022-11-15 14:15:08 +0100

    Grocery Page...

--- preparación/examen1/buy&pay/buypage.php
+++ preparación/examen1/buy&pay/buypage.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Buyment Page</title>
+    <title>Buy Page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="buypage.css">
   </head>
   <body>
-    <h1>Welcome to the Grocery Market</h1>
-    <h3>Select your products:</h3>
-    <form action="buypage.php" method="get">
-      <table>
-        <tr>
-          <td><label for "1">Banana</label><br><input type="checkbox"></td>
-          <td><label for "1">Apple</label><br><input type="checkbox"></td>
-          <td><label for "1">Milk</label><br><input type="checkbox"></td>
-          <td><label for "1">Lettuce</label><br><input type="checkbox"></td>
-        </tr>
-        <tr>
-          <td colspan="4" align="center"><input type="submit" value="Payment"></td>
-        </tr>
-      </table>
+    <div class="center">
+      <h1>Welcome to the Grocery Market</h1>
+      <h3>Select your products:</h3>
+      <form action="paypage.php" method="get">
+        <table>
+          <tr>
+            <td><label>Banana</label><br><input type="checkbox"></td>
+            <td><label for "apple">Apple</label><br><input type="checkbox"></td>
+            <td><label for "milk">Milk</label><br><input type="checkbox" value=""></td>
+            <td><label for "lettuce">Lettuce</label><br><input type="checkbox" value="lettuce"></td>
+          </tr>
+          <tr>
+            <td colspan="4" align="center"><a href="paypage.php"></a><input type="submit" value="Payment"></td>
+          </tr>
+        </table>
     </form>
+    </div>
+    <footer><P> NAH DE LOCOS</footer>
   </body>
 </html>
